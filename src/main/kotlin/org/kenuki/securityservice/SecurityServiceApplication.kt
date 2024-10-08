413a6ec367d4213fa1dc429fab215df33a157f6f
TheJustRusik <7thejustrusik7@gmail.com>
2024-10-08 23:39:16 +0500

    Initial commit

---
+++ src/main/kotlin/org/kenuki/securityservice/SecurityServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.kenuki.securityservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecurityServiceApplication>(*args)
+}
---
+++ src/test/kotlin/org/kenuki/securityservice/SecurityServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kenuki.securityservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
