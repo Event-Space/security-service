6ce38b2698ed65905a399c2d68d02af60ec1c0b1
TheJustRusik <7thejustrusik7@gmail.com>
2024-11-12 22:49:07 +0500

    feat: migrations for microservices

--- src/main/resources/db/migration/V3__user2.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE user_unit.users
-    DROP COLUMN username;
-
-ALTER TABLE user_unit.users
-    ALTER COLUMN first_name DROP NOT NULL;
-
-ALTER TABLE user_unit.users
-    ALTER COLUMN phone_number DROP NOT NULL;
